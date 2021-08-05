f644f605510dd0f03dcb36ecf0d9d4659a58dac8
xiaotengda <xiaotengda@rouchi.com>
2021-08-05 20:37:14 +0800

    数据

--- src/pages/Editor/index.jsx
+++ src/pages/Editor/index.jsx
@@ -1,7 +1,7 @@
-import {Card, Row, Col} from 'antd';
+import {Card, Row, Col, Checkbox, Form, Image} from 'antd';
 import ProForm, {ProFormUploadButton, ProFormText, ProFormCheckbox, ProFormRadio} from '@ant-design/pro-form';
 import {PageContainer} from '@ant-design/pro-layout';
-import styles from './style.less';
+import {players, genre, language, group, tags} from './options_data'
 
 export default () => {
   return <PageContainer content="添加编辑游戏">
@@ -10,89 +10,81 @@ export default () => {
         name="basic"
         layout="horizontal"
         labelCol={{span: 2}}
+        onFinish={() => {
+        }}
       >
         <ProFormText
           width="md"
           label="中文名称"
           name="chinese"
-          placeholder="游戏的名字"
+          placeholder="请输入游戏的中文名称"
+          required
         />
-
         <ProFormText
           width="md"
           label="英文名称"
           name="english"
-          placeholder="游戏的名字"
+          placeholder="请输入游戏的英文名称"
+          required
         />
-        <ProFormUploadButton
-          name="upload"
+        <ProFormText
+          width="md"
+          label="别名"
+          name="chinese"
+          placeholder="请输入游戏的其他名称，比如简称"
+        />
+        <ProFormText
+          width="md"
           label="封面"
-          max={2}
-          fieldProps={{
-            name: 'file',
-            listType: 'picture-card',
-          }}
-          action="/upload.do"
+          name="chinese"
+          placeholder="请粘贴游戏封面的网址"
+          rules={[]}
         />
+        <Row>
+          <Col offset={2}>
+            <Image width={100} height={100} src=""/></Col>
+        </Row>
         <ProFormRadio.Group
           name="radio"
           label="游玩人数"
-          options={[
-            {
-              label: 'item 1',
-              value: 'a',
-            },
-            {
-              label: 'item 2',
-              value: 'b',
-            },
-            {
-              label: 'item 3',
-              value: 'c',
-            },
-          ]}
+          options={players}
+          required
+        />
+        <ProFormCheckbox.Group
+          name="radio"
+          label="类型"
+          options={genre}
+          required
         />
         <ProFormRadio.Group
           name="radio"
-          label="类型"
-          options={[
-            {
-              label: 'item 1',
-              value: 'a',
-            },
-            {
-              label: 'item 2',
-              value: 'b',
-            },
-            {
-              label: 'item 3',
-              value: 'c',
-            },
-          ]}
+          label="语言"
+          options={language}
+          required
         />
         <ProFormRadio.Group
           name="radio"
           label="人群"
-          options={[
-            {
-              label: 'item 1',
-              value: 'a',
-            },
-            {
-              label: 'item 2',
-              value: 'b',
-            },
-            {
-              label: 'item 3',
-              value: 'c',
-            },
-          ]}
+          options={group}
+          required
         />
-        <ProFormCheckbox.Group
-          name="checkbox-group"
-          label="属性"
-          options={['A', 'B', 'C', 'D', 'E', 'F']}
-        />
+        {/*<ProFormCheckbox.Group*/}
+        {/*  name="checkbox-group"*/}
+        {/*  label="属性"*/}
+        {/*  options={tags}*/}
+        {/*/>*/}
+        <Form.Item label="属性"
+                   required>
+          <Checkbox.Group style={{width: '100%'}}>
+            <Row gutter={[0, 8]}>
+              {
+                tags.map(({label, value}) => <Col span={4} key={value}>
+                  <Checkbox value={value}>{label}</Checkbox>
+                </Col>)
+              }
+            </Row>
+          </Checkbox.Group>
+        </Form.Item>
       </ProForm>
     </Card>
   </PageContainer>
--- src/pages/Editor/options_data.js
+++ src/pages/Editor/options_data.js
@@ -1,127 +1,132 @@
 const players = [
   {
-    id: 'one',
-    name: '1'
+    value: 'one',
+    label: '1人'
   },
   {
-    id: 'one_two',
-    name: '1~2'
+    value: 'one_two',
+    label: '1~2人'
   },
   {
-    id: 'two',
-    name: '2'
+    value: 'two',
+    label: '2人'
   },
   {
-    id: 'one_four',
-    name: '1~4'
+    value: 'one_four',
+    label: '1~4人'
   },
   {
-    id: 'four',
-    name: '4'
+    value: 'four',
+    label: '4人'
   },
   {
-    id: 'one_eight',
-    name: '1~8'
+    value: 'one_six',
+    label: '1～6人'
+  },
+  {
+    value: 'one_eight',
+    label: '1~8人'
   },
 ]
 
 const genre = [
   {
-    id: 'fps',
-    name: '第一人称射击'
+    value: 'fps',
+    label: '第一人称射击'
   },
   {
-    id: 'tps',
-    name: '第三人称射击'
+    value: 'tps',
+    label: '第三人称射击'
   },
   {
-    id: 'fly',
-    name: '飞行射击'
+    value: 'fly',
+    label: '飞行射击'
   },
   {
-    id: 'rpg',
-    name: '角色扮演'
+    value: 'rpg',
+    label: '角色扮演'
   },
   {
-    id: 'arpg',
-    name: '动作角色扮演'
+    value: 'arpg',
+    label: '动作角色扮演'
   },
   {
-    id: 'act',
-    name: '动作'
+    value: 'act',
+    label: '动作'
   },
   {
-    id: 'ftg',
-    name: '格斗'
+    value: 'ftg',
+    label: '格斗'
   },
   {
-    id: 'avg',
-    name: '冒险'
+    value: 'avg',
+    label: '冒险'
   },
   {
-    id: 'puz',
-    name: '解密'
+    value: 'puz',
+    label: '解密'
   },
   {
-    id: 'rac',
-    name: '赛车'
+    value: 'rac',
+    label: '赛车'
   },
   {
-    id: 'sim',
-    name: '模拟经营'
+    value: 'sim',
+    label: '模拟经营'
   },
 ]
 
 const group = [
   {
-    id: 'lover',
-    name: '情侣',
+    value: 'lover',
+    label: '情侣',
   },
   {
-    id: '',
-    name: '闺蜜',
+    value: '',
+    label: '闺蜜',
   },
   {
-    id: 'brothers',
-    name: '好兄弟',
+    value: 'brothers',
+    label: '好兄弟',
   },
   {
-    id: 'parentage',
-    name: '亲子',
+    value: 'parentage',
+    label: '亲子',
   },
   {
-    id: 'children',
-    name: '小朋友',
+    value: 'children',
+    label: '小朋友',
   },
 ]
 
 const language = [
-  {id: 'chinese', name: '中文'},
-  {id: 'update', name: '更新后有中文'},
-  {id: 'other', name: '无中文'},
+  {value: 'chinese', label: '中文'},
+  {value: 'update', label: '更新后有中文'},
+  {value: 'other', label: '无中文'},
 ]
 
 const tags = [
-  {id: '', name: '爽快'},
-  {id: '', name: '横版过关'},
-  {id: '', name: '恐怖'},
-  {id: '', name: '打僵尸'},
-  {id: '', name: '回合制'},
-  {id: '', name: '欢乐'},
-  {id: '', name: '休闲'},
-  {id: '', name: '剧情丰富'},
-  {id: '', name: '合作'},
-  {id: '', name: '科幻'},
-  {id: '', name: '奇幻'},
-  {id: '', name: '开放世界'},
-  {id: '', name: '多人打boss'},
-  {id: '', name: '容易上手'},
-  {id: '', name: '困难'},
-  {id: '', name: '肉鸽游戏'},
-  {id: '', name: '血腥'},
-  {id: '', name: '动漫'},
-  {id: '', name: '超级英雄'},
-  {id: '', name: '电影'},
+  {value: 'shuangkuai', label: '爽快'},
+  {value: 'hengbanguoguan', label: '横版过关'},
+  {value: 'kongbu', label: '恐怖'},
+  {value: 'dajiangshi', label: '打僵尸'},
+  {value: 'huishi', label: '回合制'},
+  {value: 'huanle', label: '欢乐'},
+  {value: 'xiuxian', label: '休闲'},
+  {value: 'juqingfengfu', label: '剧情丰富'},
+  {value: 'hezuo', label: '合作'},
+  {value: 'kehuan', label: '科幻'},
+  {value: 'qihuan', label: '奇幻'},
+  {value: 'kaifangshijie', label: '开放世界'},
+  {value: 'duorendaboss', label: '多人打boss'},
+  {value: 'rongyishangshou', label: '容易上手'},
+  {value: 'kunnan', label: '困难'},
+  {value: 'rouge', label: '肉鸽游戏'},
+  {value: 'xuexing', label: '血腥'},
+  {value: 'dongman', label: '动漫'},
+  {value: 'chaojiyingxiong', label: '超级英雄'},
+  {value: 'dianying', label: '电影'},
+  {value: 'tigan', label: '体感'},
 ]
 
-export default {players, genre, group, tags, language}
+export {players, genre, group, tags, language}
