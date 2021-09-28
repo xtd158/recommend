f13913babe0bf8e87c514529c379258612a24511
xiaotengda <xiaotengda@rouchi.com>
2021-09-28 20:21:11 +0800

    electron

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,39 +1,49 @@
 export default class Store {
 
   db
-  name = 'games'
+  storeName = 'games'
+  tableName = 'list'
   version = 1
   objectStore
 
   constructor() {
-    let request = window.indexedDB.open(this.name, this.version);
-    request.onupgradeneeded = this.onupgradeneeded
-    request.onerror = this.onerror
-    request.success = this.success
+    this.request = indexedDB.open(this.storeName, this.version);
+    this.request.onupgradeneeded = this.onupgradeneeded
+    this.request.onerror = this.onerror
+    this.request.onsuccess = this.onsuccess
   }
 
-  onupgradeneeded(event) {
+  onupgradeneeded = (event) => {
     this.db = event.target.result
-    if (!this.db.objectStoreNames.contains(this.name)) {
-      this.objectStore = this.db.createObjectStore('list', {autoIncrement: true})
-      this.objectStore.createIndex('chinese_name', 'chinese_name', {unique: true});
-      this.objectStore.createIndex('english_name', 'english_name', {unique: true});
-      this.objectStore.createIndex('alias', 'alias', {unique: false});
-      this.objectStore.createIndex('number_of_people', 'number_of_people', {unique: false});
-      this.objectStore.createIndex('game_type', 'game_type', {unique: false});
-      this.objectStore.createIndex('language', 'language', {unique: false});
-      this.objectStore.createIndex('crowd', 'crowd', {unique: false});
-      this.objectStore.createIndex('attributes', 'attributes', {unique: false});
-      this.objectStore.createIndex('cover_url', 'cover_url', {unique: true});
-      this.objectStore.createIndex('cover', 'cover', {unique: true});
+    if (!this.db.objectStoreNames.contains(this.storeName)) {
+      this.objectStore = this.db.createObjectStore(this.tableName, {autoIncrement: true})
+      this.objectStore.createIndex('chinese_name', 'chinese_name', {unique: true})
+      this.objectStore.createIndex('english_name', 'english_name', {unique: true})
+      this.objectStore.createIndex('alias', 'alias', {unique: false})
+      this.objectStore.createIndex('number_of_people', 'number_of_people', {unique: false})
+      this.objectStore.createIndex('game_type', 'game_type', {unique: false})
+      this.objectStore.createIndex('language', 'language', {unique: false})
+      this.objectStore.createIndex('crowd', 'crowd', {unique: false})
+      this.objectStore.createIndex('attributes', 'attributes', {unique: false})
+      this.objectStore.createIndex('cover_url', 'cover_url', {unique: true})
+      this.objectStore.createIndex('cover', 'cover', {unique: true})
+    } else {
+      this.objectStore = this.db.objectStore(this.tableName)
     }
   }
 
   onerror(event) {
 
+    console.log('失败')
   }
 
-  success(event) {
+  onsuccess = (event) => {
+    this.db = this.request.result
+  }
 
+  add = (data) => {
+    let request = this.db.transaction([this.tableName], 'readwrite')
+      .objectStore(this.tableName)
+      .add(data)
   }
 }
